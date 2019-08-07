08326a467d514762d9f763893843b6311b84ed15
melody789 <xinyum1@andrew.cmu.edu>
2019-08-07 13:02:38 -0400

    is valid funtion

--- R/utils.R
+++ R/utils.R
@@ -20,3 +20,23 @@ generate_board_mat <- function(n =5,p=0.25 ){
   mat <- matrix(x, nrow = n)
   mat
 }
+
+
+#' check if it is a valid board
+#'
+#' @param mat, a matrix
+#'
+#' @return true or error
+#' @export
+#'
+#' @examples is_valid(c(matrix(c(1,2,3,4), nrow = 2)))
+#' is_valid(generate_board_mat())
+#' is_valid(generate_board_mat(n=1))
+is_valid <-function(mat){
+  assert_that(nrow(mat)==ncol(mat))
+  assert_that(all(mat %in% c(0, 1, 2)))
+  assert_that(all(mat%%1==0) == TRUE)
+  assert_that(is.matrix(mat))
+  return (TRUE)
+}
+
