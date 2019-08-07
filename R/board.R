0f1edcc583eac8c3da64d351a3950ff6dbd76b2c
melody789 <xinyum1@andrew.cmu.edu>
2019-08-07 13:13:20 -0400

    add board

---
+++ R/board.R
@@ -0,0 +1,26 @@
+#' make a board object
+#'
+#' @param mat  the board mastrix has default value n=5, p=0.25
+#' @param n  number of trails
+#' @param p  probality
+#'
+#' @return  an object that is borad
+#' @export
+#'
+#' @examples board(5, 0.25)
+board <- function(mat = NULL, n =5 , p=0.25){
+  if (!is.null(mat)) {
+    is_valid(mat)
+    object <- mat
+    attr(object, "n") <- ncol(mat)
+    attr(object, "p") <- sum(mat == 0)/(nrow(mat)^2)
+  }
+  else {
+    mat2 <- generate_board_mat(n,p)
+    object <- mat2
+    attr(object, "trails") <- n
+    attr(object, "prob") <- p
+  }
+  class(object) <- c("matrix","board")
+  object
+}
